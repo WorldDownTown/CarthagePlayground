86c6f21f8a31f71547ab3177060fe5a520b0a7c4
WorldDownTown <WorldDownTown@gmail.com>
2017-02-23 14:45:35 +0900

    initial commit.

---
+++ CarthagePlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import PlaygroundSupport
+
+let liveView: UIView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
+liveView.backgroundColor = .white
+PlaygroundPage.current.liveView = liveView
---
+++ CarthagePlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarthagePlayground
+//
+//  Created by Keisuke Shoji on 2017/02/23.
+//  Copyright © 2017年 VASILY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
